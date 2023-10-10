4d412ad0689a0994c9d58a91fb1088480c4cf3a9
Chris Atchison <chris.atcho@gmail.com>
2023-10-10 18:14:12 +1100

    tg

--- terraform/lb.tf
+++ terraform/lb.tf
@@ -12,11 +12,11 @@ resource "aws_lb_target_group" "main" {
   vpc_id      = module.vpc.vpc_id
 }
 
-resource "aws_lb_target_group_attachment" "main" {
-  target_group_arn = aws_lb_target_group.main.arn
-  target_id        = aws_autoscaling_group.main.id
-  port             = 80
-}
+# resource "aws_lb_target_group_attachment" "main" {
+#   target_group_arn = aws_lb_target_group.main.arn
+#   target_id        = aws_autoscaling_group.main.id
+#   port             = 80
+# }
 
 resource "aws_lb_listener" "main" {
   load_balancer_arn = aws_lb.main.arn
