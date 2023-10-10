f9ef5def0d34f8f527e46fb4f585405c134cf8b7
Chris Atchison <chris.atcho@gmail.com>
2023-10-10 17:37:35 +1100

    .

--- terraform/lb.tf
+++ terraform/lb.tf
@@ -6,7 +6,7 @@ resource "aws_lb" "main" {
 }
 
 resource "aws_lb_target_group" "main" {
-  target_type = "alb"
+  target_type = "instance"
   port        = 80
   protocol    = "HTTP"
   vpc_id      = module.vpc.vpc_id
