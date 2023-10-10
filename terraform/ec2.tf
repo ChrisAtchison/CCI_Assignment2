b605440bf13679ebb511dbc9491c111c1d588156
Chris Atchison <chris.atcho@gmail.com>
2023-10-10 18:18:15 +1100

    .

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -26,7 +26,7 @@ resource "aws_autoscaling_group" "main" {
   availability_zones = module.vpc.azs
   desired_capacity   = 2
   max_size           = 2
-  min_size           = 1
+  min_size           = 2
   target_group_arns = [
     aws_lb_target_group.main.arn,
   ]
